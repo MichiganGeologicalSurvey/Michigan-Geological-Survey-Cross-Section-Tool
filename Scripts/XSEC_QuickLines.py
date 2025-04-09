2f40a21c1f87c6c9210a53612f7acaf9cc548fce
mbell-migeo <matthew.e.bell@wmich.edu>
2025-04-09 10:00:41 -0400

    MGS Tool Update - 4/9/2025

--- Scripts/XSEC_QuickLines.py
+++ Scripts/XSEC_QuickLines.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
         except:
             pass
         if elevField == "":
-            pass
+            newElevField = "DEM_ELEV"
         else:
             newElevField = elevField
 
